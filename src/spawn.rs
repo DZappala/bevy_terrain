e7f71b32afc2a99d234933b90e520e8d33a38356
Dominic Zappala <dominic.zappala@gmail.com>
2025-03-13 20:26:49 -0400

    chore: remove redundant lifetiems

--- src/spawn.rs
+++ src/spawn.rs
@@ -100,7 +100,10 @@ pub trait SpawnTerrainCommandsExt<M: Material> {
     );
 }
 
-impl<'w, 's, M: Material> SpawnTerrainCommandsExt<M> for Commands<'w, 's> {
+impl<M> SpawnTerrainCommandsExt<M> for Commands<'_, '_>
+where
+    M: Material,
+{
     fn spawn_terrain(
         &mut self,
         config: Handle<TerrainConfig>,
